d880bd3f00cd47b401ebf2112e0db8b23a534f27
Will Luo <willluo@rlec106-dsdev-uswest2.udsrvs.net>
2020-09-18 17:10:34 -0700

    factored out the redis client

--- Scripts/redis_get_set.py
+++ Scripts/redis_get_set.py
@@ -7,63 +7,7 @@ This script will use locust as framework.
 Author:- OpsTree Solutions
 """
 
-import json
-import time
-from locust import Locust, User
-from locust import between, events, task
-import redis
-import gevent.monkey
-gevent.monkey.patch_all()
-from random import randint
-
-def load_config(filepath):
-    """For loading the connection details of Redis"""
-    with open(filepath) as property_file:
-        configs = json.load(property_file)
-    return configs
-
-filename = "redis.json"
-
-configs = load_config(filename)
-
-class RedisClient(object):
-    def __init__(self, host=configs['redis_host'], port=configs['redis_port'], pw=configs['redis_password'], cert=configs['redis_cert']):
-        self.rc = redis.StrictRedis(host=host, port=port, password=pw, ssl=True, ssl_certfile=cert, ssl_cert_reqs=None, ssl_check_hostname=False)
-
-    
-    def query(self, key, command='GET'):
-        """Function to Test GET operation on Redis"""
-        result = None
-        start_time = time.time()
-        try:
-            result = self.rc.get(key)
-            if not result:
-                result = ''
-        except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
-            events.request_failure.fire(request_type=command, name=key, response_time=total_time, response_length=0, exception=e)
-        else:
-            total_time = int((time.time() - start_time) * 1000)
-            length = len(result)
-            events.request_success.fire(request_type=command, name=key, response_time=total_time, response_length=length)
-        return result
-
-    def write(self, key, value, command='SET'):
-        """Function to Test SET operation on Redis"""
-        result = None
-        start_time = time.time()
-        try:
-            result=self.rc.set(key,value)
-            if not result:
-                result = ''
-        except Exception as e:
-            total_time = int((time.time() - start_time) * 1000)
-            events.request_failure.fire(request_type=command, name=key, response_time=total_time, response_length=0, exception=e)
-        else:
-            total_time = int((time.time() - start_time) * 1000)
-            length = 1
-            events.request_success.fire(request_type=command, name=key, response_time=total_time, response_length=length)
-        return result
+from locust_redis_client import *
 
 class RedisLocust(User):
     def __init__(self, *args, **kwargs):
